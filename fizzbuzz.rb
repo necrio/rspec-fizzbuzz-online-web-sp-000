54441f582107f11ee36c9d55c15220154ecf43bd
Keith Isham <kisham8899@gmail.com>
2019-04-06 17:06:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,13 @@
 # See README.md for instructions on how to do this
 #def fizzbuzz(int)
 
-  if int % 3 == 0 # if the number is divisible by 3
+#  if int % 3 == 0 # if the number is divisible by 3
     "fizz" #Go fizz
   #else int % 5 == 0
   #elsif int % 15 == 0
 
-  end
+#  end
+#end
+def fizzbuzz(int)
+  number
 end
