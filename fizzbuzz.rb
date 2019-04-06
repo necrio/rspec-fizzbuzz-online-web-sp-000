28263221540259b27d50a431b12b153eb7ef4cd9
Keith Isham <kisham8899@gmail.com>
2019-04-06 17:03:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,8 @@ def fizzbuzz(int)
 
   if int % 3 == 0 # if the number is divisible by 3
     "fizz" #Go fizz
-  else int % 5 == 0
-  elsif int % 15 == 0
+  #else int % 5 == 0
+  #elsif int % 15 == 0
 
   end
   fizz(3)
